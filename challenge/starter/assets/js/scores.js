f43e2f6acf1e9b0d2d1eafb6e9208f098ef644aa
Fillipe <you@example.com>
2023-11-25 16:56:37 +0000

    pages changing done, issue with local storage, can't retrieve data to display on the screen

--- challenge/starter/assets/js/scores.js
+++ challenge/starter/assets/js/scores.js
@@ -1,9 +1,55 @@
 // New page to manage score and 2 final screens.
-endScreenEL = document.getElementById('end-screen');
+var endScreenEl = document.getElementById('end-screen');
+var finalscoreEl = document.getElementById("final-score");
+var submitButton = document.querySelector('#submit');
+var initialsInput = document.querySelector("#initials");
+var highScoresSpan = document.querySelector("#highscores");
+
+
 
 function endScreen(){
     choices.setAttribute('class', 'hide');
-    endScreenEL.setAttribute('class', 'show');
-
+    endScreenEl.setAttribute('class', 'show');
+    finalscoreEl.innerHTML = timerCount;
 
 }
+
+//
+//function highScores(){
+//    
+//    location.href = 'highscores.html';
+//    var email = localStorage.getItem("initials");
+//    var finalScore= localStorage.getItem("score");
+//    
+//
+//    highScoresSpan.textContent = email + finalScore;
+//
+//   
+//
+//}
+//
+
+
+
+
+
+submitButton.addEventListener('click',function(event) {
+    event.preventDefault();
+    var initials = document.querySelector("#initials").value;
+    var finalScore = timerCount;
+   
+
+localStorage.setItem("initials", initials);
+localStorage.setItem("score",finalScore);
+
+
+location.href = 'highscores.html';
+var email = localStorage.getItem("initials");
+var finalScore= localStorage.getItem("score");
+
+
+highScoresSpan.textContent = email + finalScore;
+
+
+
+});
